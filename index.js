1b4abbb96bf0ea02d30a5c4e5e6b0cbd4ca6f85d
luobotang <tangkangxing@sina.com>
2015-08-29 22:10:56 +0800

    add init check

--- index.js
+++ index.js
@@ -2,41 +2,50 @@ var $ = require('jquery');
 
 var ViewImage = require('./lib/viewer-image');
 
-var KEY_ESC = 27;
+var KEY_ESC = 27
+var CLS_ON_IMAGE_VIEWING = 'on-image-viewing'
 
 // 图片全屏预览组件
 
 var box = $("<div>", {
 	"class": "image-viewer-container",
 	click: close
-});
-
-var view_img;
-
-function show(img_src) {
-	view_img = new ViewImage(img_src);
-	box.append(view_img);
-	box.show();
-	$('body').addClass('on-image-viewing');
-};
-
-function close() {
-	if (view_img) { // clear img
-		$(view_img).remove();
-		view_img = null;
-	}
-	box.hide();
-	$('body').removeClass('on-image-viewing');
-};
-
-// add handler on box no use, don't know why
-$(document).keydown(function (e) {
-	if (e.which == KEY_ESC) close();
 })
 
-box.hide().appendTo(document.body);
+var view_img
+var inited = false
+
+function show(img_src) {
+	if (!inited) {
+		$(function () {
+			show(img_src)
+		})
+		return
+	}
+	view_img = ViewImage(img_src);
+	box.append(view_img);
+	box.show();
+	$('body').addClass(CLS_ON_IMAGE_VIEWING);
+}
+
+function close() {
+	if (view_img) {
+		view_img.remove()
+		view_img = null
+	}
+	box.hide()
+	$('body').removeClass(CLS_ON_IMAGE_VIEWING);
+}
+
+$(function () {
+	inited = true
+	$(document).keydown(function (e) {
+		if (e.which == KEY_ESC) close()
+	})
+	box.hide().appendTo('body')
+})
 
 module.exports = {
 	show: show,
 	close: close
-};
+}
